8ebb832d4969a5fa0374dd6fd7e3746b07dcf961
AloreBB <kevincastrillon843@gmail.com>
2023-01-20 16:44:58 -0500

    Mapear una petición HTTP, accedimos a una api para concocer las películas recientes. Mapear el resultado para acceder de manera rápida y sencilla

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,7 +1,14 @@
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:peliculas/models/models.dart';
 
 class MoviesProvider extends ChangeNotifier {
+
+  String _apiKey   = '235fa3ef9bf82ad9f7e36e68229ca3d5';
+  String _baseUrl  = 'api.themoviedb.org';
+  String _language = 'es-ES';
 
   MoviesProvider() {
     print('MoviesProvider inicializado');
@@ -9,7 +16,17 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
-  }
+    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+        'api_key': _apiKey,
+        'language': _language,
+        'page': '1'
+        }
+      );
 
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+    print( nowPlayingResponse.results[1].title );
+
+  }
 }
