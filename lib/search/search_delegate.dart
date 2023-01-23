a48a3b68d687a13cd4445859df44fb6545fc8f4a
AloreBB <kevincastrillon843@gmail.com>
2023-01-23 16:13:26 -0500

    Implementamos buscador

---
+++ lib/search/search_delegate.dart
@@ -0,0 +1,51 @@
+
+
+import 'package:flutter/material.dart';
+
+class MovieSearchDelegate extends SearchDelegate {
+
+  @override
+  // TODO: implement searchFieldLabel
+  String? get searchFieldLabel => 'Buscar película';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () => query = '', 
+        icon: const Icon( Icons.clear )
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      }, 
+      icon: const Icon( Icons.arrow_back )
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Text('buildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    
+    if( query.isEmpty) {
+      return Container(
+        child: Center(
+          child: Icon( Icons.movie_creation_outlined, color: Colors.black38, size: 100,),
+        ),
+      );
+    }
+
+    return Container();
+
+  }
+
+}
