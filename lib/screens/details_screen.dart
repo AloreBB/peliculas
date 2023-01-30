9b0c28e9881665963eee775daa7556ae4d1169df
AloreBB <kevincastrillon843@gmail.com>
2023-01-30 17:21:58 -0500

    agregamos efecto Hero, relaciona widgets para hacer una herrrrrmosa transicion

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -98,12 +98,15 @@ class _PosterAndTitles extends StatelessWidget {
       padding: const EdgeInsets.symmetric( horizontal: 20 ),
       child: Row(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: FadeInImage(
-              placeholder: const AssetImage('assets/no-image.jpg'),
-              image: NetworkImage(movie.fullPosterImg),
-              height: 150,
+          Hero(
+            tag: movie.id,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: const AssetImage('assets/no-image.jpg'),
+                image: NetworkImage(movie.fullPosterImg),
+                height: 150,
+              ),
             ),
           ),
 
